81912b24240df547d6cffd88b9eb083510be3f99
Shenelle_Peart <speart@limoanywhere.com>
2020-11-09 21:33:12 -0600

    Added Logic to filter component

--- src/App.js
+++ src/App.js
@@ -13,6 +13,25 @@ function App() {
 
   const sortProducts = (event) => {
     // console.log(event.target.value);
+    const sort = event.target.value
+    setInventoryDetails((inventoryDetails) => ({
+      sort: sort,
+      products: inventoryDetails.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id > b._id
+            ? 1
+            : -1
+        ),
+    }));
   };
 
   const filterProducts = (event) => {
