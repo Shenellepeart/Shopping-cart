7ef901b0f0623c2f8ac89c5bae26422318646c42
Shenelle_Peart <speart@limoanywhere.com>
2020-11-09 23:50:41 -0600

    Added UI for Cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,8 @@
 import React from "react";
+import "./Cart.css";
+import formatCurrency from "../utities";
 
-const Cart = ({ cartItems }) => {
+const Cart = ({ cartItems, removeFromCart }) => {
   return (
     <div>
       {cartItems.length === 0 ? (
@@ -10,6 +12,26 @@ const Cart = ({ cartItems }) => {
           You have {cartItems.length} in the cart
         </div>
       )}
+      <div>
+        <div className="cart">
+          <ul className="cart-items">
+            {cartItems.map((item) => (
+              <li key={item._id}>
+                <div>
+                  <img src={item.image} alt={item.title} />
+                </div>
+                <div>
+                  <div>{item.title}</div>
+                  <div className="price">
+                    {formatCurrency(item.price)} X {item.count}
+                    <button className="button" onClick={() => removeFromCart(item)}>Remove</button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
