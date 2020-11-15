71a6085008d455c741014a82fc8e927c148ec294
Shenelle_Peart <speart@limoanywhere.com>
2020-11-14 19:09:14 -0600

    integrated redux into products component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchProducts } from "../actions/productActions";
 import "./Products.css";
 import formatCurrency from "../utities";
 
-const Products = ({ inventory, addToCart }) => {
-  return (
+const Products = ({ products, addToCart, fetchProducts }) => {
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return products.loading ? (
+    <h2>loading...</h2>
+  ) : products.error ? (
+    <h2>{products.error}</h2>
+  ) : (
     <div>
       <ul className="products">
-        {inventory?.products?.map((product) => (
-          <li key={product._id}>
+        {products?.items?.map((item) => (
+          <li key={item._id}>
             <div className="product">
-              <a href={`#${product._id}`}>
-                <img src={product.image} alt={product.title} />
-                <p>{product.title}</p>
+              <a href={`#${item._id}`}>
+                <img src={item.image} alt={item.title} />
+                <p>{item.title}</p>
               </a>
               <div className="product-price">
-                <div>{formatCurrency(product.price)}</div>
-                <button className="button primary" onClick={()=>addToCart(product)}>
+                <div>{formatCurrency(item.price)}</div>
+                <button
+                  className="button primary"
+                  onClick={() => addToCart(item)}
+                >
                   Add To Cart
                 </button>
               </div>
@@ -27,4 +40,16 @@ const Products = ({ inventory, addToCart }) => {
   );
 };
 
-export default Products;
+const mapStateToProps = (state) => {
+  return {
+    products: state.products,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchProducts: () => {
+    dispatch(fetchProducts());
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
